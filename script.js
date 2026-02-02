c1d829cc42ffd87404a88c62839f5e61eea6650e
yassine <90511259+yassine261001@users.noreply.github.com>
2026-02-02 23:41:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let buttons = document.querySelectorAll(".Cell")
 let resetButton = document.getElementById("ResetButton")
-
+let clearBoard = document.getElementById("ClearBoard")
 let playerTurn = "X"
+let currentTurn = document.getElementById("CurrentTurnSymbol")
+let resetScores = document.getElementById("ResetScores")
+let xscore = document.getElementById("X-Score")
+let oscore = document.getElementById("O-Score")
+
+currentTurn.innerHTML = playerTurn
 
 function PlayerTurns () {
 buttons.forEach(button => {
@@ -10,20 +16,45 @@ buttons.forEach(button => {
                 button.innerHTML = "✕",
                 button.disabled = true,
                 playerTurn = "O"
+                currentTurn.innerHTML = playerTurn
             } else {
                 button.innerHTML = "◯",
                 button.disabled = true,
                 playerTurn = "X"
-            } checkWinnerX(), checkWinnerO();
+                currentTurn.innerHTML = playerTurn
+            } checkWinnerX(), checkWinnerO(); checkDraw();
     }); 
 })
 } PlayerTurns();
+
+resetScores.addEventListener("click", () => {
+    xscore.innerHTML = 0;
+    oscore.innerHTML = 0;
+    winner.innerHTML = "The Scores have been reset"
+})
+
+clearBoard.addEventListener("click", () => {
+    buttons.forEach(button => {
+        button.innerHTML = "";
+        button.disabled = false;
+        winner.innerHTML = "Board cleared"
+    })
+})
 
 resetButton.addEventListener("click", () => {
     buttons.forEach(button => {
         button.innerHTML = "";
         button.disabled = false;
-        winner.innerHTML = "Game started!";
+        currentTurn.innerHTML = playerTurn
+        if (playerTurn == "X") {
+            playerTurn = "O"
+            currentTurn.innerHTML = playerTurn
+            winner.innerHTML = "Turn switched to ◯";
+        } else {
+            playerTurn = "X"
+            currentTurn.innerHTML = playerTurn
+            winner.innerHTML = "Turn switched to ✕"
+        }
     });
 });
 
@@ -38,7 +69,6 @@ let cell8 = document.getElementById("cell8");
 let cell9 = document.getElementById("cell9");
 let winner = document.getElementById("WinnerDisplay");
 
-
 function checkWinnerX() {
 if (cell1.textContent == "✕" && cell2.textContent == "✕" && cell3.textContent == "✕" || 
     cell4.textContent == "✕" && cell5.textContent == "✕" && cell6.textContent == "✕" ||
@@ -49,7 +79,7 @@ if (cell1.textContent == "✕" && cell2.textContent == "✕" && cell3.textConten
     cell1.textContent == "✕" && cell5.textContent == "✕" && cell9.textContent == "✕" ||
     cell3.textContent == "✕" && cell5.textContent == "✕" && cell7.textContent == "✕"
 ) {
-    winner.innerHTML="X won this round. Click restart to start the next one.";
+    winner.innerHTML="X won this round. Clear the board and start the next round.";
     let xscore = document.getElementById("X-Score");
     let xvalue = parseInt(xscore.innerHTML)+1;
     xscore.innerHTML = xvalue;
@@ -72,4 +102,14 @@ if (cell1.textContent == "◯" && cell2.textContent == "◯" && cell3.textConten
     oscore.innerHTML = ovalue;
     }
 }
+
+function checkDraw () {
+    if (cell1.textContent != "" && cell2.textContent != "" &&
+        cell3.textContent != "" && cell4.textContent != "" &&
+        cell5.textContent != "" && cell6.textContent != "" &&
+        cell7.textContent != "" && cell8.textContent != "" &&
+        cell9.textContent != "") {
+        winner.innerHTML = "It's a Draw! Clear the board to proceed."
+        }
+};
 
